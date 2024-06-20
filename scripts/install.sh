ff37857faa954c34ca08ba33124c31092118c87e
Full Pointer Exception <fullpointerxception@gmail.com>
2024-06-20 09:56:44 -0500

    Added scripts to copy to/from the /home/jdurham/.config directory neovim is looking for

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ./init.lua $HOME/.config/nvim
+cp ./lazy-lock.json $HOME/.config/nvim
+cp -r ./after $HOME/.config/nvim
+cp -r ./colors $HOME/.config/nvim
+cp -r ./lua $HOME/.config/nvim
+cp -r ./plugin $HOME/.config/nvim
+cp -r ./scripts $HOME/.config/nvim
