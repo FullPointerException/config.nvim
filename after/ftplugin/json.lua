df76a9bfff8f986d99a8ab62271bf7af183f1c72
Full Pointer Exception <fullpointerxception@gmail.com>
2024-06-27 09:37:12 -0500

    Added json ftplugin options

---
+++ after/ftplugin/json.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+-- Tabs are 4 spaces
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- Don't add comments on "o" or newlines
+opt.formatoptions:remove "o"
+opt.formatoptions:remove "r"
